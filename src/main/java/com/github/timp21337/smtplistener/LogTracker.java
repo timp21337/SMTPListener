e010000180affd22a5e12e46241c3e234801c889
timp21337 <timp@paneris.org>
2013-12-20 17:58:41 +0000

    Looks prettier in cobertura

--- src/main/java/com/github/timp21337/smtplistener/LogTracker.java
+++ src/main/java/com/github/timp21337/smtplistener/LogTracker.java
@@ -5,11 +5,13 @@ public class LogTracker {
   private static Integer line = 0;
 
   public static String number(String in) {
+    String out = null;
     synchronized (line) {
       line++;
       if (line == 100)
         line = 1;
-      return (line < 10 ? " " : "") + line  + " " + in;
+      out = (line < 10 ? " " : "") + line  + " " + in;
     }
+    return out;
   }
 }
