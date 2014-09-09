ee7f1cd4a404db7d2d250cb587350fb676d0c2fa
timp21337 <timp21337@paneris.org>
2014-09-09 15:03:39 +0100

    Coverage

--- src/test/java/smtplistener/LogTrackerTest.java
+++ src/test/java/smtplistener/LogTrackerTest.java
@@ -3,6 +3,7 @@ package smtplistener;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Method;
 
 import junit.framework.TestCase;
 
@@ -28,4 +29,27 @@ public class LogTrackerTest extends TestCase {
     constructor.setAccessible(true);
     constructor.newInstance();
   }
+
+  public void testPrivateConstructors() throws Exception {
+    excercisePrivateConstuctor(LogTracker.class);
+  }
+
+  public static void excercisePrivateConstuctor(final Class<?> clazz)
+      throws NoSuchMethodException, InvocationTargetException,
+      InstantiationException, IllegalAccessException {
+    assertTrue("There must be only one constructor", clazz.getDeclaredConstructors().length == 1);
+    final Constructor<?> constructor = clazz.getDeclaredConstructor();
+    assertFalse("The constructor is accessible", constructor.isAccessible());
+    assertTrue("The constructor is not private", Modifier.isPrivate(constructor.getModifiers()));
+    constructor.setAccessible(true);
+    constructor.newInstance();
+    constructor.setAccessible(false);
+    for (final Method method : clazz.getMethods()) {
+      if (method.getDeclaringClass().equals(clazz)) {
+        assertTrue("There exists a non-static method:" + method,
+            Modifier.isStatic(method.getModifiers()));
+      }
+    }
+  }
+
 }
